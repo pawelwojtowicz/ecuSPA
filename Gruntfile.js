af1dfdcd2e548ae14ce32e43b5784e2f02ddf335
pawel.w <elendil@konto.pl>
2016-11-06 16:39:39 +0100

    grunt has been added - good

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	htmlhint: {
+	  options: {
+		"tag-pair": true
+	  },
+	  html1: {
+		src: ['index.html','partials/*.html']
+	  }
+	},
+	jshint: {
+    all: ['js/*.js']
+	},
+	concat: {
+    options: {
+      stripBanners: true,
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+    },
+    dist: {
+      src: ['js/controllers.js','js/app.js'],
+      dest: 'dist/built.js',
+    }
+  },
+  uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'dist/built.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+  }
+    
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');  
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('all', ['htmlhint','jshint','concat','uglify']);
+
+};
